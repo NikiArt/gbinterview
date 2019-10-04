0ca18110acb8a4c8d56c3bcb048c71cf5de5279f
Nikita Boiko <genertyniki@gmail.com>
2019-10-04 05:59:57 +0300

    add simple numbers

--- src/main/java/lessonthree/SimpleNumbers.java
+++ src/main/java/lessonthree/SimpleNumbers.java
@@ -11,7 +11,7 @@ public class SimpleNumbers {
     private static List<Integer> getSimpleNumbers() {
         List<Integer> list = new ArrayList<>();
         for (int i = 2; i < 1000; i++) {
-            Boolean canAttached = true;
+            boolean canAttached = true;
             for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
                     canAttached = false;
